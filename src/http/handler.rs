9948b6f842d06fa55c9746d27be785a79aadd479
Shikai Liu <lsk569937453@gmail.com>
2024-05-19 17:04:31 +0800

    Add the head request for http.

--- src/http/handler.rs
+++ src/http/handler.rs
@@ -1,3 +1,4 @@
+use crate::cli::app_config::Cli;
 use futures::StreamExt;
 use http_body_util::BodyExt;
 use http_body_util::BodyStream;
@@ -11,52 +12,54 @@ use std::cmp::min;
 use std::fs::OpenOptions;
 use std::io::Write as WriteStd;
 
-pub async fn handle_response(
-    file_path_option: Option<String>,
-    res: Response<Incoming>,
-) -> Result<(), anyhow::Error> {
-    let content_length = res
-        .headers()
-        .get(CONTENT_LENGTH)
-        .ok_or(anyhow!("Can not parse content_length!"))?
-        .to_str()?
-        .parse::<u64>()?;
+pub async fn handle_response(cli: &Cli, res: Response<Incoming>) -> Result<(), anyhow::Error> {
+    if cli.header_option {
+        println!("{:?} {}", res.version(), res.status());
+        for (key, value) in res.headers().iter() {
+            println!("{}: {}", key, value.to_str()?);
+        }
+        return Ok(());
+    }
+    let content_length_option = res.headers().get(CONTENT_LENGTH);
+    if let Some(content_lenth_str) = content_length_option {
+        let content_length = content_lenth_str.to_str()?.parse::<u64>()?;
 
-    if let Some(file_path) = file_path_option {
-        let incoming = res.into_body();
-        let mut body_streaming = BodyStream::new(incoming);
-        let mut downloaded = 0;
-        let total_size = content_length;
-        let mut file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open(file_path)?;
-        let pb = ProgressBar::new(total_size);
-        pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        if let Some(file_path) = cli.file_path_option.clone() {
+            let incoming = res.into_body();
+            let mut body_streaming = BodyStream::new(incoming);
+            let mut downloaded = 0;
+            let total_size = content_length;
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(file_path)?;
+            let pb = ProgressBar::new(total_size);
+            pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
         ?
         .progress_chars("#>-"));
 
-        while let Some(Ok(t)) = body_streaming.next().await {
-            if let Ok(bytes) = t.into_data() {
-                let new = min(downloaded + bytes.len() as u64, total_size);
-                downloaded = new;
-                pb.set_position(new);
-                file.write_all(&bytes)?;
+            while let Some(Ok(t)) = body_streaming.next().await {
+                if let Ok(bytes) = t.into_data() {
+                    let new = min(downloaded + bytes.len() as u64, total_size);
+                    downloaded = new;
+                    pb.set_position(new);
+                    file.write_all(&bytes)?;
+                }
             }
-        }
-        pb.finish_with_message("downloaded");
-    } else {
-        if content_length > 1024 * 1024 * 100 {
-            return Err(anyhow!(
-                "Binary output can mess up your terminal. Use '--output -' to tell
+            pb.finish_with_message("downloaded");
+        } else {
+            if content_length > 1024 * 1024 * 100 {
+                return Err(anyhow!(
+                    "Binary output can mess up your terminal. Use '--output -' to tell
 rcurl to output it to your terminal anyway, or consider '--output
 <FILE>' to save to a file."
-            ));
+                ));
+            }
+            let mut body = res.collect().await?.aggregate();
+            let dst = body.copy_to_bytes(content_length as usize);
+            let response_string = String::from_utf8_lossy(&dst);
+            println!("{}", response_string);
         }
-        let mut body = res.collect().await?.aggregate();
-        let dst = body.copy_to_bytes(content_length as usize);
-        let response_string = String::from_utf8_lossy(&dst);
-        println!("{}", response_string);
     }
 
     Ok(())
