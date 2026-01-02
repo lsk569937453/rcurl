5cae92a136f865264b8112a1b75cdbc63db5de34
shikai liu <lsk569937453@gmail.com>
2026-01-02 14:50:15 +0800

    Refactor: replace Pin box future with BoxFuture type alias

--- src/http/proxy.rs
+++ src/http/proxy.rs
@@ -149,11 +149,12 @@ impl HttpForwardProxyConnector {
         Self { proxy_addr }
     }
 }
+use futures::future::BoxFuture;
 
 impl Service<Uri> for HttpForwardProxyConnector {
     type Response = TokioIo<TcpStream>;
     type Error = anyhow::Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
     fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
